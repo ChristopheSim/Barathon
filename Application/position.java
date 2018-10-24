64a3db15d6191abfec94ea0cd0548ee1df5723ff
2a6m <maxime.bourguignon1996@gmail.com>
2018-10-24 10:40:33 +0200

    complete class Position

--- Application/position.java
+++ Application/position.java
@@ -1,2 +1,22 @@
 public class Position {
+
+    private Double longitude;
+    private Double latitude;
+
+    public Position(Double lo, Double la) {
+        this.longitude = lo;
+        this.latitude = la;
+    }
+
+    public GetLongitude() {
+        return this.longitude;
+    }
+
+    public GetLatitude() {
+        return this.latitude;
+    }
+
+    public IsEqually(Position p) {
+        // to implement
+    }
 }
