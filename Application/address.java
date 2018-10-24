a70f0df2d298129e3fce3f9dfcb052ff6c6ec904
2a6m <maxime.bourguignon1996@gmail.com>
2018-10-24 10:29:26 +0200

    complete class Address

--- Application/address.java
+++ Application/address.java
@@ -1,2 +1,24 @@
 public class Address {
+
+    private String street;
+    private String number;
+    private Position position;
+
+    public Address(String s, String n, Position p) {
+        this.street = s;
+        this.number = n;
+        this.Position = p;
+    }
+
+    public getStreet() {
+        return this.street;
+    }
+
+    public GetPosition() {
+        return this.position;
+    }
+
+    public GetNumber() {
+        return this.number;
+    }
 }
