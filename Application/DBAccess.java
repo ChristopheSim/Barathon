63adcfe837b959cd7e7f25a4543a472001e821fa
ChristopheSim <c.christophesimon@gmail.com>
2018-11-05 14:50:46 +0100

    Add the template of DBAccess

---
+++ Application/DBAccess.java
@@ -0,0 +1,16 @@
+public class DBAccess {
+  public void open() {
+  }
+
+  public void close() {
+  }
+
+  public void create(Place place) {
+  }
+
+  public void update(Place place) {
+  }
+
+  public void delete(Place place) {
+  }
+}
