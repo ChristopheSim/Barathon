d2e9aa8bcafefbf7e91303c13ca0c76dc44dcfe4
ChristopheSim <c.christophesimon@gmail.com>
2018-11-06 22:27:28 +0100

    Update some functions

--- Application/DBAccess.java
+++ Application/DBAccess.java
@@ -1,11 +1,21 @@
+// This class is used to connect the database (neo4j) with the application Barathon
+
+
 public class DBAccess {
   public void open() {
+	Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "NEO4J"));
   }
 
   public void close() {
+	try (Session session = driver.session()) {
+		driver.close();
+	}
   }
 
   public void create(Place place) {
+	try (Session session = driver.session()) {
+    		StatementResult rs = session.run("CREATE (" + place + ") RETURN " + place);
+	}
   }
 
   public void update(Place place) {
