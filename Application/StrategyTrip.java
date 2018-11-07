3e9ba97d15a6afbc33013a43a200da89de09767c
2a6m <maxime.bourguignon1996@gmail.com>
2018-11-07 10:32:38 +0100

    ceate class StrategyTrip

---
+++ Application/StrategyTrip.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class StrategyTrip extends Strategy {
+    public ArrayList<Place> solve(Position p, Caracteristics c) {
+        ArrayList<Place> lst = new ArrayList<Place>();
+        return lst;
+    }
+}
