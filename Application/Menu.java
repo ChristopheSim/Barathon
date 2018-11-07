407feb31101f5bdc3bc6557b3ba57e27204099ed
Jordan <jordan@PC-JORDAN.localdomain>
2018-11-07 09:23:21 +0100

    Menu.java : added getters GetMeanXXXPrice() to avoid computing the meanPrice everytime if it is already up to date

--- Application/Menu.java
+++ Application/Menu.java
@@ -5,28 +5,38 @@ public class Menu {
 
 	private HashSet<Food> foods;
 	private HashSet<Drink> drinks;
+	private int meanDrinkPrice;
+	private int meanFoodPrice;
+	private boolean meanDrinkPriceUTD;
+	private boolean meanFoodPriceUTD;
 
 	public Menu () {
 
 		this.foods = new HashSet<Food> ();
 		this.drinks = new HashSet<Drink> ();
+		this.meanDrinkPriceUTD = false;
+		this.meanFoodPriceUTD = false;
 
 	}
 
 	public void AddFood (Food f) {
 		foods.add(f);
+		this.meanFoodPriceUTD = false;
 	}
 
 	public void RemoveFood (Food f) {
 		foods.remove(f);
+		this.meanFoodPriceUTD = false;
 	}
 
 	public void AddDrink (Drink d) {
 		drinks.add(d);
+		this.meanDrinkPriceUTD = false;
 	}
 
 	public void RemoveDrink (Drink d) {
 		drinks.remove(d);
+		this.meanDrinkPriceUTD = false;
 	}
 
 	private int MeanComponentPrice (HashSet<? extends Component> components) {
@@ -40,11 +50,33 @@ public class Menu {
 		return sum/components.size();
 	}
 
-	public int MeanFoodPrice () {
-		return MeanComponentPrice(foods);	
+	private int MeanFoodPrice () {
+		this.meanFoodPrice = MeanComponentPrice(foods); 
+		this.meanFoodPriceUTD = true;
+		return this.meanFoodPrice;
 	}
 
-	public int MeanDrinkPrice () {
-		return MeanComponentPrice(drinks);	
+	private int MeanDrinkPrice () {
+		this.meanDrinkPrice = MeanComponentPrice(drinks);
+		this.meanDrinkPriceUTD = true;
+		return this.meanDrinkPrice;	
+	}
+
+	public int GetMeanFoodPrice () {
+		if (this.meanFoodPriceUTD) {
+			return this.meanFoodPrice;
+		}
+		else {
+			return this.MeanFoodPrice();
+		}
+	}
+
+	public int GetMeanDrinkPrice () {
+		if (this.meanDrinkPriceUTD) {
+			return this.meanDrinkPrice;
+		}
+		else {
+			return this.MeanDrinkPrice();
+		}
 	}
 }
