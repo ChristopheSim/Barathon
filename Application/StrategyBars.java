ea74e6478cbf889d79373e05d8a5a5cd200cf1ea
2a6m <maxime.bourguignon1996@gmail.com>
2018-11-07 10:32:03 +0100

    create class StrategyBar

---
+++ Application/StrategyBars.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class StrategyBars extends Strategy {
+    public ArrayList<Place> solve(Position p, Caracteristics c) {
+        ArrayList<Place> lst = new ArrayList<Place>();
+        return lst;
+    }
+}
