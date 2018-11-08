33bd8025d5974754c0faf02681f0359fcf53bfdc
ChristopheSim <c.christophesimon@gmail.com>
2018-11-08 13:27:21 +0100

    example test and test2 ok

--- Application/DBAccess.java
+++ Application/DBAccess.java
@@ -1,26 +1,19 @@
 // This class is used to connect the database (neo4j) with the application Barathon
 
+// imports
+import org.neo4j.driver.v1.*;
 
-public class DBAccess {
-  public void open() {
-	Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "NEO4J"));
-  }
 
-  public void close() {
-	try (Session session = driver.session()) {
-		driver.close();
+public final class DBAccess {
+  public static void createPlace(Place place) {
+    Driver driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("neo4j", "NEO4J"));
+    try (Session session = driver.session()) {
+      // To complete with all the properties of a node place
+      StatementResult rs = session.run(String.format("CREATE (:Place {name: '%s'})", place.getName()));
+    }
+    catch(Exception e) {
+      System.out.println("An error occured !");
+    }
+    driver.close();
 	}
-  }
-
-  public void create(Place place) {
-	try (Session session = driver.session()) {
-    		StatementResult rs = session.run("CREATE (" + place + ") RETURN " + place);
-	}
-  }
-
-  public void update(Place place) {
-  }
-
-  public void delete(Place place) {
-  }
 }
---
+++ Application/Test2.java
@@ -0,0 +1,6 @@
+public class Test2 {
+  public static void main(String[] args) {
+    String node_test = "test";
+    Test.create(node_test);
+  }
+}
