a6c1fe94b32aded0a30abb150ae63b7d7a8e5e9e
ChristopheSim <c.christophesimon@gmail.com>
2018-11-12 15:14:10 +0100

    Completet the Creation of the DB

--- Application/CreateDB.java
+++ Application/CreateDB.java
@@ -1,10 +1,59 @@
 public final class CreateDB {
-  public static void createBaseDB() {
+  public static void db() {
     // Create the constraints of the graph
     DBAccess.createUniqueConstraints();
 
     // Create the caracteristics in the graph
     DBAccess.createCaracteristics();
+
+    // Create the sample of drinks
+    Drink coca = new Drink();
+    Drink duvel = new Drink();
+
+    // Create the sample of food
+    Food salad = new Food();
+    Food wafel = new Food();
+
+    // Create the menus
+    Menu menu1 = new Menu();
+    Menu menu2 = new Menu();
+    Menu menu3 = new Menu();
+
+    // Create the caracteristics
+    Caracteristics carac1 = new Caracteristics();
+    Caracteristics carac2 = new Caracteristics();
+    Caracteristics carac3 = new Caracteristics();
+
+    // Create the preferences
+    Preferences pref1 = new Preferences();
+    Preferences pref2 = new Preferences();
+
+    //Create the positions
+    Position posp1 = new Position();
+    Position posp2 = new Position();
+    Position posp3 = new Position();
+    Position posp4 = new Position();
+    Position posp5 = new Position();
+    Position posp6 = new Position();
+    Position posp7 = new Position();
+    Position posp8 = new Position();
+    Position posp9 = new Position();
+    Position posp10 = new Position();
+
+    Position posu1 = new Position();
+    Position posu2 = new Position();
+
+    // Create the addresses
+    Address addp1 = new Address();
+    Address addp2 = new Address();
+    Address addp3 = new Address();
+    Address addp4 = new Address();
+    Address addp5 = new Address();
+    Address addp6 = new Address();
+    Address addp7 = new Address();
+    Address addp8 = new Address();
+    Address addp9 = new Address();
+    Address addp10 = new Address();
 
     // Create some places in the graph
     List<Place> places = new ArrayList<Place>();
@@ -18,11 +67,6 @@ public final class CreateDB {
     Place p8 = new Place();
     Place p9 = new Place();
     Place p10 = new Place();
-    Place p11 = new Place();
-    Place p12 = new Place();
-    Place p13 = new Place();
-    Place p14 = new Place();
-    Place p15 = new Place();
     places.add(p1);
     places.add(p2);
     places.add(p3);
@@ -33,19 +77,12 @@ public final class CreateDB {
     places.add(p8);
     places.add(p9);
     places.add(p10);
-    places.add(p11);
-    places.add(p12);
-    places.add(p13);
-    places.add(p14);
-    places.add(p15);
 
     // Create some users in the graph
     List<User> users = new ArrayList<User>();
     User u1 = new User();
     User u2 = new User();
-    User u3 = new User();
     users.add(u1);
     users.add(u2);
-    users.add(u3);
   }
 }
