c142c5a2445a1854b8c750e898fb000ba06562d1
ChristopheSim <c.christophesimon@gmail.com>
2018-11-12 15:41:22 +0100

    Add the structure of the Test DBAccess

--- Application/Test_DBAccess.java
+++ Application/Test_DBAccess.java
@@ -1,6 +1,18 @@
 public class Test_DBAccess {
   public static void main(String[] args) {
-    // Test the different functions of the 
+    // Creation of the database
+    CreateDB.db();
+
+
+    // Test the different functions
+    /*
+    - The nearest bar;
+    - The list of bars where it is possible to eat in a range of Y kms;
+    - The list of the X nearest bar in a range of Y kms;
+    */
+    UseDB.NeraestBar();
+    UseDB.BarToEat(int Y);
+    UseDB.NearbyBars(int X, int Y);
 
   }
 }
