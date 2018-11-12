102ae673e85a1f9620e83d236743d044279ec31e
ChristopheSim <c.christophesimon@gmail.com>
2018-11-12 11:44:04 +0100

    Complete the Test2 file

--- Application/Test2.java
+++ Application/Test2.java
@@ -1,6 +1,12 @@
 public class Test2 {
   public static void main(String[] args) {
-    String node_test = "test";
-    Test.create(node_test);
+    //String node_test = "test";
+    //Test.create(node_test);
+
+    String node_test1 = "test3";
+    Test.create(node_test1);
+
+    String node_test2 = "test2";
+    Test.create(node_test2);
   }
 }
