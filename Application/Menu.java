a7e1fbc8fa8c42049220301d32286e6432920af1
ChristopheSim <c.christophesimon@gmail.com>
2018-11-14 10:41:19 +0100

    Fix the Menu class

--- Application/Menu.java
+++ Application/Menu.java
@@ -1,10 +1,9 @@
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.*;
 
 public class Menu {
 
-	private HashSet<Food> foods;
-	private HashSet<Drink> drinks;
+	private ArrayList<Food> foods;
+	private ArrayList<Drink> drinks;
 	private int meanDrinkPrice;
 	private int meanFoodPrice;
 	private boolean meanDrinkPriceUTD;
@@ -12,37 +11,45 @@ public class Menu {
 
 	public Menu () {
 
-		this.foods = new HashSet<Food> ();
-		this.drinks = new HashSet<Drink> ();
+		this.foods = new ArrayList<Food> ();
+		this.drinks = new ArrayList<Drink> ();
 		this.meanDrinkPriceUTD = false;
 		this.meanFoodPriceUTD = false;
 
 	}
 
 	public void AddFood (Food f) {
-		foods.add(f);
+		this.foods.add(f);
 		this.meanFoodPriceUTD = false;
 	}
 
 	public void RemoveFood (Food f) {
-		foods.remove(f);
+		this.foods.remove(f);
 		this.meanFoodPriceUTD = false;
 	}
 
+	public ArrayList<Food> getFoods() {
+		return this.foods;
+	}
+
 	public void AddDrink (Drink d) {
-		drinks.add(d);
+		this.drinks.add(d);
 		this.meanDrinkPriceUTD = false;
 	}
 
 	public void RemoveDrink (Drink d) {
-		drinks.remove(d);
+		this.drinks.remove(d);
 		this.meanDrinkPriceUTD = false;
 	}
-	
+
+	public ArrayList<Drink> getDrinks() {
+		return this.drinkss;
+	}
+
 	// Compute the mean price of a list of components - drinks or foods
 	private int MeanComponentPrice (HashSet<? extends Component> components) {
 		int sum = 0;
-		
+
 		// Adds the price of each element in the list
 		for (Iterator<? extends Component> c = components.iterator(); c.hasNext();) {
 			sum += c.next().GetPrice();
@@ -53,18 +60,18 @@ public class Menu {
 
 	// Compute the mean price
 	private int MeanFoodPrice () {
-		this.meanFoodPrice = MeanComponentPrice(foods); 
+		this.meanFoodPrice = MeanComponentPrice(foods);
 		this.meanFoodPriceUTD = true;
 		return this.meanFoodPrice;
 	}
-	
+
 	// Compute the mean price
 	private int MeanDrinkPrice () {
 		this.meanDrinkPrice = MeanComponentPrice(drinks);
 		this.meanDrinkPriceUTD = true;
-		return this.meanDrinkPrice;	
+		return this.meanDrinkPrice;
 	}
-	
+
 	// Get the mean price of foods - Do not compute it if up-to-date
 	public int GetMeanFoodPrice () {
 		if (this.meanFoodPriceUTD) {
