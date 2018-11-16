ed5fd5faa219f8ce7e815656b1f6c8eee7a7e574
ChristopheSim <c.christophesimon@gmail.com>
2018-11-16 17:31:51 +0100

    Resolve some conflicts in Menu class

--- Application/src/main/java/Menu.java
+++ Application/src/main/java/Menu.java
@@ -16,10 +16,12 @@ public class Menu {
 
 	public void addFood (Food f) {
 		this.foods.add(f);
+		this.setMeanFoodPrice();
 	}
 
 	public void removeFood (Food f) {
 		this.foods.remove(f);
+		this.setMeanFoodPrice();
 	}
 
 	public ArrayList<Food> getFoods() {
@@ -28,18 +30,27 @@ public class Menu {
 
 	public void addDrink (Drink d) {
 		this.drinks.add(d);
+		this.setMeanDrinkPrice();
 	}
 
 	public void removeDrink (Drink d) {
 		this.drinks.remove(d);
+		this.setMeanDrinkPrice();
 	}
 
 	public ArrayList<Drink> getDrinks() {
-		return this.drinkss;
+		return this.drinks;
 	}
 
 	public void setMeanFoodPrice() {
-		this.meanFoodPrice = computeMeanPrice(this.foods);
+		int sum = 0;
+
+		// Adds the price of each element in the list
+		for (Food f : this.foods) {
+			sum += f.getPrice();
+		}
+
+		this.meanFoodPrice = sum/this.foods.size();
 	}
 
 	public int getMeanFoodPrice() {
@@ -47,20 +58,28 @@ public class Menu {
 	}
 
 	public void setMeanDrinkPrice() {
-		this.meanDrinkPrice = computeMeanPrice(this.drink);
+		int sum = 0;
+
+		// Adds the price of each element in the list
+		for (Drink d : this.drinks) {
+			sum += d.getPrice();
+		}
+
+		this.meanDrinkPrice = sum/this.drinks.size();;
 	}
+
 	public int getMeanDrinkPrice() {
 		return this.meanDrinkPrice;
 	}
 
-	// Compute the mean price of a list of components - drinks or foods
-	private int compiteMeanPrice (ArrayList<Component> components) {
+	/* Compute the mean price of a list of components - drinks or foods
+	private int computeMeanPrice (ArrayList<Component> components) {
 		int sum = 0;
 
 		// Adds the price of each element in the list
-		foreach (Component c in components) {
+		for (Component c : components) {
 			sum += c.getPrice();
 		}
 		return sum/components.size();
-	}
+	}*/
 }
