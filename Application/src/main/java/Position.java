9f204a2c7d9c7c3a6ba46ff79955892fecad8742
ChristopheSim <c.christophesimon@gmail.com>
2018-11-16 17:32:32 +0100

    Resolve some conflicts in Posiotion class

--- Application/src/main/java/Position.java
+++ Application/src/main/java/Position.java
@@ -5,7 +5,7 @@ public class Position {
     private Double longitude;
     private Double latitude;
 
-    public void Position(Double lo, Double la) {
+    public Position(Double lo, Double la) {
         this.longitude = lo;
         this.latitude = la;
     }
