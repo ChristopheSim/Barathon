89909a30acd8681f9fa960293b6b07cf3b19957f
Jordan-JACOBI <32301879+Jordan-JACOBI@users.noreply.github.com>
2018-11-20 10:08:47 +0100

    price changed from double to int

--- Application/src/test/java/MenuTest.java
+++ Application/src/test/java/MenuTest.java
@@ -5,15 +5,15 @@ public class DrinkTest {
 	@Before
 	public void InitClasses() {
 		
-		Drink d1 = new Drink("Drink", 1.0, 0, false);
-		Drink d2 = new Drink("Drink", 2.0, 0, false);
-		Drink d3 = new Drink("Drink", 3.0, 0, false);
-		Drink d4 = new Drink("Drink", 4.0, 0, false);
+		Drink d1 = new Drink("Drink", 100, 0, false);
+		Drink d2 = new Drink("Drink", 200, 0, false);
+		Drink d3 = new Drink("Drink", 300, 0, false);
+		Drink d4 = new Drink("Drink", 400, 0, false);
 
-		Food f1 = new Food("Food", 1.0, false, false, false);
-		Food f2 = new Food("Food", 2.0, false, false, false);
-		Food f3 = new Food("Food", 3.0, false, false, false);
-		Food f4 = new Food("Food", 4.0, false, false, false);
+		Food f1 = new Food("Food", 100, false, false, false);
+		Food f2 = new Food("Food", 200, false, false, false);
+		Food f3 = new Food("Food", 300, false, false, false);
+		Food f4 = new Food("Food", 400, false, false, false);
 	}
 
 	@Test
@@ -78,33 +78,33 @@ public class DrinkTest {
 	public void TestGetMeanDrinkPrice() {		
 		Menu m = new Menu();
 		
-		assertEquals(0.00, m.getMeanDrinkPrice(), 2);
+		assertEquals(0, m.getMeanDrinkPrice());
 		m.addDrink(d1);
-		assertEquals(1.00, m.getMeanDrinkPrice(), 2);
+		assertEquals(100, m.getMeanDrinkPrice());
 		m.addDrink(d2);
-		assertEquals(1.50, m.getMeanDrinkPrice(), 2);
+		assertEquals(150, m.getMeanDrinkPrice());
 		m.addDrink(d3);
-		assertEquals(2.00, m.getMeanDrinkPrice(), 2);
+		assertEquals(200, m.getMeanDrinkPrice());
 		m.addDrink(d4);
-		assertEquals(2.50, m.getMeanDrinkPrice(), 2);
+		assertEquals(250, m.getMeanDrinkPrice());
 		m.removeDrink(d4);
-		assertEquals(2.00, m.getMeanDrinkPrice(), 2);
+		assertEquals(200, m.getMeanDrinkPrice());
 	}
 	
 	@Test
 	public void TestGetMeanFoodPrice() {		
 		Menu m = new Menu();
 
-		assertEquals(0.00, m.getMeanFoodPrice(), 2);
+		assertEquals(0, m.getMeanFoodPrice());
 		m.addFood(f1);
-		assertEquals(1.00, m.getMeanFoodPrice(), 2);
+		assertEquals(100, m.getMeanFoodPrice());
 		m.addFood(f2);
-		assertEquals(1.50, m.getMeanFoodPrice(), 2);
+		assertEquals(150, m.getMeanFoodPrice());
 		m.addFood(f3);
-		assertEquals(2.00, m.getMeanFoodPrice(), 2);
+		assertEquals(200, m.getMeanFoodPrice());
 		m.addFood(f4);
-		assertEquals(2.50, m.getMeanFoodPrice(), 2);
+		assertEquals(250, m.getMeanFoodPrice());
 		m.removeFood(f4);
-		assertEquals(2.00, m.getMeanFoodPrice(), 2);
+		assertEquals(200, m.getMeanFoodPrice());
 	}
 }
