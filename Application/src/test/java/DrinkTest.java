25b3be4295f02c1bba18d8616545aa4a8377c4ff
ChristopheSim <c.christophesimon@gmail.com>
2018-11-20 15:32:23 +0100

    Change the Drink tests

--- Application/src/test/java/DrinkTest.java
+++ Application/src/test/java/DrinkTest.java
@@ -8,14 +8,14 @@ public class DrinkTest {
 
 		Assert.assertFalse((new Drink("Drink", 10, 9.7, false)).getSoft());
 		Assert.assertFalse((new Drink("Drink", 10, 9.7, true)).getSoft());
-		Assert.assertFalse((new Drink("Drink", 85, 40, false)).getSoft());
-		Assert.assertFalse((new Drink("Drink", 85, 40, true)).getSoft());
+		Assert.assertFalse((new Drink("Drink", 85, 40.0, false)).getSoft());
+		Assert.assertFalse((new Drink("Drink", 85, 40.0, true)).getSoft());
 		Assert.assertFalse((new Drink("Drink", 10, 121.3789, false)).getSoft());
 		Assert.assertFalse((new Drink("Drink", 10, 121.3789, true)).getSoft());
-		Assert.assertTrue((new Drink("Drink", 0, 0, false)).getSoft());
-		Assert.assertTrue((new Drink("Drink", 0, 0, true)).getSoft());
-		Assert.assertTrue((new Drink("Drink", 10, -5, false)).getSoft());
-		Assert.assertTrue((new Drink("Drink", 10, -5, true)).getSoft());
+		Assert.assertTrue((new Drink("Drink", 0, 0.0, false)).getSoft());
+		Assert.assertTrue((new Drink("Drink", 0, 0.0, true)).getSoft());
+		Assert.assertFalse((new Drink("Drink", 10, -5.0, false)).getSoft());
+		Assert.assertFalse((new Drink("Drink", 10, -5.0, true)).getSoft());
 
 	}
 }
