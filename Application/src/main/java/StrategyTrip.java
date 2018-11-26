ba97afc39c7e56d59eeacfa79a58c0db243e6169
ChristopheSim <c.christophesimon@gmail.com>
2018-11-26 16:39:14 +0100

    Remove the test DBAccess

--- Application/src/main/java/StrategyTrip.java
+++ Application/src/main/java/StrategyTrip.java
@@ -1,6 +1,16 @@
 import java.util.ArrayList;
 
+/**
+ * Class to find the trip of bars.
+ */
 public class StrategyTrip implements Strategy {
+  /**
+   *  Method solve.
+   *
+   * @param p position of the user.
+   * @param c carcateristics of the user.
+   * @return list of places.
+   */
     public ArrayList<Place> solve(Position p, Caracteristics c) {
         ArrayList<Place> lst = new ArrayList<Place>();
         // Takes the parameter position and return a trip
