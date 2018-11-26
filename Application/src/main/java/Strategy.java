5d0ec556fd3424d48fc329352156864550b52ee7
ChristopheSim <c.christophesimon@gmail.com>
2018-11-26 16:30:36 +0100

    Correction checkstyles

--- Application/src/main/java/Strategy.java
+++ Application/src/main/java/Strategy.java
@@ -9,6 +9,7 @@ interface Strategy {
      *
      * @param p position of the user.
      * @param c carcateristics of the user.
+     * @return list of places.
      */
     public ArrayList<Place> solve(Position p, Caracteristics c);
 }
