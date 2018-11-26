e4be31ffd62f637fc1b619dc9e637530028a646a
ChristopheSim <c.christophesimon@gmail.com>
2018-11-26 16:46:03 +0100

    Javadoc Strategy checkstyle

--- Application/src/main/java/StrategyBars.java
+++ Application/src/main/java/StrategyBars.java
@@ -12,7 +12,7 @@ public class StrategyBars implements Strategy {
    * @param c carcateristics of the user.
    * @return list of places.
    */
-    ArrayList<Place> solve(final Position p, final Caracteristics c) {
+    public ArrayList<Place> solve(final Position p, final Caracteristics c) {
         ArrayList<Place> lst = new ArrayList<Place>();
         // Takes the parameter position and returns all the bars that in 100km
         // around the position
--- Application/src/main/java/StrategyTrip.java
+++ Application/src/main/java/StrategyTrip.java
@@ -11,7 +11,7 @@ public class StrategyTrip implements Strategy {
    * @param c carcateristics of the user.
    * @return list of places.
    */
-   ArrayList<Place> solve(final Position p, final Caracteristics c) {
+   public ArrayList<Place> solve(final Position p, final Caracteristics c) {
         ArrayList<Place> lst = new ArrayList<Place>();
         // Takes the parameter position and return a trip
         // Ordered list of places that matches with the caracteristics
