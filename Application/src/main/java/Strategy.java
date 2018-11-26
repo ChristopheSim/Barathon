ad4555d3259834be13d60e0b5fa530b1a30969f8
ChristopheSim <c.christophesimon@gmail.com>
2018-11-26 16:27:52 +0100

    Correction checkstyles

--- Application/src/main/java/Strategy.java
+++ Application/src/main/java/Strategy.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
 
+/**
+ *  Interface for the strategy pattern.
+ */
 interface Strategy {
-    ArrayList<Place> solve(Position p, Caracteristics c);
+    /**
+     *  Method solve.
+     *
+     * @param p position of the user.
+     * @param c carcateristics of the user.
+     */
+    public ArrayList<Place> solve(Position p, Caracteristics c);
 }
