6bb5547062cb46defeb88606b34894b678bd7f15
ChristopheSim <c.christophesimon@gmail.com>
2018-11-26 14:37:18 +0100

    Change the component test to improve the isEqual method

--- Application/src/test/java/ComponentTest.java
+++ Application/src/test/java/ComponentTest.java
@@ -8,17 +8,17 @@ public class ComponentTest {
 
 		Assert.assertTrue((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 10, 5.2, true))));
 		Assert.assertFalse((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Beer", 10, 5.2, true))));
-		Assert.assertTrue((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 8, 5.2, true))));
-		Assert.assertTrue((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 10, 0.1, true))));
-		Assert.assertTrue((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 10, 5.2, false))));
+		Assert.assertFalse((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 8, 5.2, true))));
+		Assert.assertFalse((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 10, 0.1, true))));
+		Assert.assertFalse((new Drink("Drink", 10, 5.2, true)).isEqual((new Drink("Drink", 10, 5.2, false))));
 
 		Assert.assertFalse((new Drink("Drink", 10, 5.2, true)).isEqual((new Food("Food", 10, true, true, true))));
 
 		Assert.assertTrue((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, true, true, true))));
 		Assert.assertFalse((new Food("Food", 10, true, true, true)).isEqual((new Food("Hamburger", 10, true, true, true))));
-		Assert.assertTrue((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 12, true, true, true))));
-		Assert.assertTrue((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, false, true, true))));
-		Assert.assertTrue((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, true, false, true))));
-		Assert.assertTrue((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, true, true, false))));
+		Assert.assertFalse((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 12, true, true, true))));
+		Assert.assertFalse((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, false, true, true))));
+		Assert.assertFalse((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, true, false, true))));
+		Assert.assertFalse((new Food("Food", 10, true, true, true)).isEqual((new Food("Food", 10, true, true, false))));
 	}
 }
