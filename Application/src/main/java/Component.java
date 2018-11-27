082e1f9bae8776dbd0683bd57de64f57773f336a
2a6m <maxime.bourguignon1996@gmail.com>
2018-11-27 10:26:44 +0100

    correction visibility

--- Application/src/main/java/Component.java
+++ Application/src/main/java/Component.java
@@ -1,6 +1,6 @@
 /**
- *	This class represents the components to be added to a menu
- *	This is the super class of Food and Drink
+ *	This class represents the components to be added to a menu.
+ *	This is the super class of Food and Drink.
  */
 public abstract class Component {
 
@@ -8,10 +8,10 @@ public abstract class Component {
 	private int price;
 
 	/**
-	 * Constructor
+	 * Constructor.
 	 *
-	 * @param n The name of the component
-	 * @param p The price of the component
+	 * @param n The name of the component.
+	 * @param p The price of the component.
 	 */
 	public Component(String n, int p) {
 
@@ -21,10 +21,10 @@ public abstract class Component {
 	}
 
 	/**
-	 *	This method checks if 2 Components are equals
+	 *	This method checks if 2 Components are equals.
 	 *
-	 * @param c Component
-	 * @return boolean TRUE if the 2 Components are equals
+	 * @param c Component.
+	 * @return boolean TRUE if the 2 Components are equals.
 	 */
 	public boolean isEqual(Component c) {
 		//Need further implementation
@@ -33,36 +33,36 @@ public abstract class Component {
 	}
 
 	/**
-	 * Method to get the component's name
+	 * Method to get the component's name.
 	 *
-	 * @return string representing the component's name
+	 * @return string representing the component's name.
 	 */
 	public String getName() {
 		return this.name;
 	}
 
 	/**
-	 * Method to get the component's price
+	 * Method to get the component's price.
 	 *
-	 * @return int representing the component's price
+	 * @return int representing the component's price.price
 	 */
 	public int getPrice() {
 		return this.price;
 	}
 
 	/**
-	 * Method to set the component's name
+	 * Method to set the component's name.
 	 *
-	 * @param name string representing the component's name
+	 * @param name string representing the component's name.
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	/**
-	 * Method to set the component's price
+	 * Method to set the component's price.
 	 *
-	 * @param price int representing the component's price
+	 * @param price int representing the component's price.
 	 */
 	public void setPrice(int price) {
 		this.price = price;
