b2cf1c80039f3855d31faf8dcaffbebc4ddc5410
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 09:52:50 +0100

    Correct class to respect the checkstyle

--- Application/src/main/java/Address.java
+++ Application/src/main/java/Address.java
@@ -1,36 +1,85 @@
-public class Address {
+/**
+ * Class to represent an address.
+ */
 
+public class Address {
+    /**
+    * street.
+    */
     private String street;
+    /**
+    * number.
+    */
     private String number;
+    /**
+    * object position.
+    */
     private Position position;
 
-    public Address(String s, String n, Position p) {
+    /**
+     * Method to generate an address.
+     *
+     * @param s street.
+     * @param n number.
+     * @param p position.
+     */
+    public Address(final String s, final String n, final Position p) {
         this.street = s;
         this.number = n;
         this.position = p;
     }
 
+    /**
+     * Method to get the street.
+     *
+     * @return a string representing the street.
+     */
     public String getStreet() {
         return this.street;
     }
 
-    public void setStreet(String s) {
+    /**
+     * Method to set the street.
+     *
+     * @param s a string representing the street.
+     */
+    public void setStreet(final String s) {
         this.street = s;
     }
 
+    /**
+     * Method to get the position.
+     *
+     * @return a object position.
+     */
     public Position getPosition() {
         return this.position;
     }
 
-    public void setPosition(Position p) {
+    /**
+     * Method to set the position.
+     *
+     * @param p a object position.
+     */
+    public void setPosition(final Position p) {
         this.position = p;
     }
 
+    /**
+     * Method to get the number.
+     *
+     * @return a string representing the number.
+     */
     public String getNumber() {
         return this.number;
     }
 
-    public void setNumber(String n) {
+    /**
+     * Method to set the number.
+     *
+     * @param n a string representing the number.
+     */
+    public void setNumber(final String n) {
         this.number = n;
     }
 }
