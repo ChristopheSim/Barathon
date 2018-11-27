f53077163556f1c07aefe71bd1487ff10e16d5eb
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 17:03:02 +0100

    Correct classes to validate checkstyle

--- Application/src/main/java/Drink.java
+++ Application/src/main/java/Drink.java
@@ -11,65 +11,62 @@ public class Drink extends Component {
      */
     private boolean sparkling;
 
-	/**
-	 * Constructor.
-	 *
-	 * @param n The name of the drink.
-	 * @param p The price of the drink.
-	 * @param a The alcochol % of the drink.
-	 * @param s If the drink is sparkling : true.
-	 */
-	public Drink(final String n, final  int p, final double a,
-	 final boolean s) {
-
+    /**
+    * Constructor.
+    *
+    * @param n The name of the drink.
+    * @param p The price of the drink.
+    * @param a The alcochol % of the drink.
+    * @param s If the drink is sparkling : true.
+    */
+    public Drink(final String n, final  int p, final double a,
+    final boolean s) {
 		super(n, p);
-
 		this.alcohol = a;
 		this.sparkling = s;
-
 	}
 
-	/**
-	 * Method to get the drink's alcohol percent.
-	 *
-	 * @return double percent of alcohol.
-	 */
+    /**
+    * Method to get the drink's alcohol percent.
+    *
+    * @return double percent of alcohol.
+    */
 	public final double getAlcohol() {
 		return this.alcohol;
 	}
 
 	/**
-	 * Method to get the drink's sparkliung.
-	 *
-	 * @return boolean true if sparkling.
-	 */
+    * Method to get the drink's sparkliung.
+    *
+    * @return boolean true if sparkling.
+    */
 	public final boolean getSparkling() {
 		return this.sparkling;
 	}
 
 	/**
-	 * Method to set the percent of alcohol.
-	 *
-	 * @param a percent of alcohol.
-	 */
+	* Method to set the percent of alcohol.
+	*
+	* @param a percent of alcohol.
+	*/
 	public final void setAlcohol(final double a) {
 		this.alcohol = a;
 	}
 
 	/**
-	 * Method to set the sparkling.
-	 *
-	 * @param s boolean of sparkling.
-	 */
+	* Method to set the sparkling.
+	*
+	* @param s boolean of sparkling.
+	*/
 	public final void setSparkling(final boolean s) {
 		this.sparkling = s;
 	}
 
 	/**
-	 * This method checks if the drink is alcohol-free.
-	 *
-	 * @return boolean TRUE if there is no alcohol.
-	 */
+	* This method checks if the drink is alcohol-free.
+	*
+	* @return boolean TRUE if there is no alcohol.
+	*/
 	public final boolean getSoft() {
 		// A drink is soft if it contains no alcohol
 		try {
