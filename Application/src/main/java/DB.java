c1c411273aa977e80e90d9b0fcf1314bada99856
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 18:13:44 +0100

    Correct classes to validate checkstyle

--- Application/src/main/java/DB.java
+++ Application/src/main/java/DB.java
@@ -1,5 +1,8 @@
 // imports
 import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Record;
 import java.util.List;
 import java.util.ArrayList;
 
