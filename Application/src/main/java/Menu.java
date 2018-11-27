a97e07a6e911654fa0e4e4a8caa7d058fd89e056
2a6m <maxime.bourguignon1996@gmail.com>
2018-11-27 11:10:01 +0100

    correction checkstyle

--- Application/src/main/java/Menu.java
+++ Application/src/main/java/Menu.java
@@ -1,143 +1,156 @@
 import java.util.*;
 
 /**
-*	This class represents the menu of a place
+*	This class represents the menu of a place.
 */
 public class Menu {
-
-	private ArrayList<Food> foods;
-	private ArrayList<Drink> drinks;
-	private int meanDrinkPrice;
-	private int meanFoodPrice;
+	/**
+	 * @param foods List of menu's food.
+	 */
+	private final ArrayList<Food> foods;
+	/**
+	 * @param drinks List of menu's drink.
+	 */
+	private final ArrayList<Drink> drinks;
+	/**
+	 * @param meanDrinkPrice mean price of drinks.
+	 */
+	private final int meanDrinkPrice;
+	/**
+	 * @param meanFoodPrice mean price of food.
+	 */
+	private final int meanFoodPrice;
 
 	/**
-	*	Constructor
-	*
-	*	The menu is empty when created
-	*/
-	public Menu () {
+	 *	Constructor.
+	 */
+	public Menu() {
 
 		this.foods = new ArrayList<Food> ();
 		this.drinks = new ArrayList<Drink> ();
 
 	}
 
-	//GETTERS
-	public int getMeanFoodPrice() {
+	/**
+	 * Method to get the food's mean price.
+	 *
+	 * @return int mean price.
+	 */
+	public final int getMeanFoodPrice() {
 		return this.meanFoodPrice;
 	}
 
-	public int getMeanDrinkPrice() {
+	/**
+	 * Method to get the drink's mean price.
+	 *
+	 * @return int mean price.
+	 */
+	public final int getMeanDrinkPrice() {
 		return this.meanDrinkPrice;
 	}
 
 	/**
-	*	This method gets all the foods from the menu
-	*
-	* @return ArrayList<Food> Food from the menu
-	*/
-	public ArrayList<Food> getFoods() {
+	 *	This method gets all the foods from the menu.
+	 *
+	 * @return ArrayList<Food> Food from the menu.
+	 */
+	public final ArrayList<Food> getFoods() {
 		return this.foods;
 	}
 
 	/**
-	*	This method gets all the drinks from the menu
-	*
-	* @return ArrayList<Drink> Drink from the menu
-	*/
-	public ArrayList<Drink> getDrinks() {
+	 *	This method gets all the drinks from the menu.
+	 *
+	 * @return ArrayList<Drink> Drink from the menu.
+	 */
+	public final ArrayList<Drink> getDrinks() {
 		return this.drinks;
 	}
 
 	/**
-	*	This method adds a food to the menu
-	*
-	* @param f Food to be added
-	*/
-	public void addFood (Food f) {
+	 *	This method adds a food to the menu.
+	 *
+	 * @param f Food to be added.
+	 */
+	public final void addFood(final Food f) {
 		this.foods.add(f);
 		this.setMeanFoodPrice();
 	}
 
 	/**
-	*	This method removes a food from the menu
-	*
-	* @param f Food to be removed
-	*/
-	public void removeFood (Food f) {
+	 *	This method removes a food from the menu.
+	 *
+	 * @param f Food to be removed.
+	 */
+	public final void removeFood(final Food f) {
 		this.foods.remove(f);
 		this.setMeanFoodPrice();
 	}
 
 
 	/**
-	*	This method adds a drink to the menu
-	*
-	* @param d Drink to be added
-	*/
-	public void addDrink (Drink d) {
+	 *	This method adds a drink to the menu.
+	 *
+	 * @param d Drink to be added.
+	 */
+	public final void addDrink(final Drink d) {
 		this.drinks.add(d);
 		this.setMeanDrinkPrice();
 	}
 
 	/**
-	*	This method removes a drink from the menu
-	*
-	* @param d Drink to be removed
-	*/
-	public void removeDrink (Drink d) {
+	 *	This method removes a drink from the menu.
+	 *
+	 * @param d Drink to be removed.
+	 */
+	public final void removeDrink(final Drink d) {
 		this.drinks.remove(d);
 		this.setMeanDrinkPrice();
 	}
 
 	/**
-	*	Compute the price of the foods
-	*/
-
-	public void setMeanFoodPrice() {
+	 *	Compute the price of the foods.
+	 */
+	public final void setMeanFoodPrice() {
 		int sum = 0;
 
 		// Adds the price of each element in the list
 		for (Food f : this.foods) {
 			sum += f.getPrice();
 		}
-
 		if (this.foods.size() > 0) {
 			this.meanFoodPrice = sum/this.foods.size();
-		}
-		else {
+		} else {
 			this.meanFoodPrice = 0;
 		}
 	}
 
 
 	/**
-	*	Compute the price of the drinks
-	*/
-	public void setMeanDrinkPrice() {
+	 *	Compute the price of the drinks.
+	 */
+	public final void setMeanDrinkPrice() {
 		int sum = 0;
 
 		// Adds the price of each element in the list
 		for (Drink d : this.drinks) {
 			sum += d.getPrice();
 		}
-
 		if (this.drinks.size() > 0) {
 			this.meanDrinkPrice = sum/this.drinks.size();
-		}
-		else {
+		} else {
 			this.meanDrinkPrice = 0;
 		}
 	}
 
 	/**
-	* Compute the mean price of a list of components - drinks or foods
-	*
-	* @param components The full list of the components
-	*
-	* @return int mean price of the components
-	*/
-	private int computeMeanPrice (ArrayList<Component> components) {
+	 * Compute the mean price of a list of components - drinks or foods.
+	 *
+	 * @param components The full list of the components.
+	 *
+	 * @return int mean price of the components.
+	 */
+	private final int computeMeanPrice(final ArrayList<Component> components) {
 		int sum = 0;
 
 		// Adds the price of each element in the list
