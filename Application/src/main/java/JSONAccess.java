936a5c5ab6127b7234128d5bea74b11fa3846a42
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 17:58:40 +0100

    Correct classes to validate checkstyle

--- Application/src/main/java/JSONAccess.java
+++ Application/src/main/java/JSONAccess.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.io.*;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
@@ -15,7 +14,7 @@ public final class JSONAccess {
     **/
     private JSONAccess() {
     }
-	
+
     /**
     * Instance of utility Gson.
     */
