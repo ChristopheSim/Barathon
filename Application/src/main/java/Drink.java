a3a79ad8573c1a9e1a7d69426cbb33e5958e12a1
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 17:12:45 +0100

    Correct classes to validate checkstyle

--- Application/src/main/java/Drink.java
+++ Application/src/main/java/Drink.java
@@ -35,51 +35,51 @@ public class Drink extends Component {
         return this.alcohol;
     }
 
-	/**
+    /**
     * Method to get the drink's sparkliung.
     *
     * @return boolean true if sparkling.
     */
-	public final boolean getSparkling() {
-		return this.sparkling;
-	}
+    public final boolean getSparkling() {
+        return this.sparkling;
+    }
 
-	/**
-	* Method to set the percent of alcohol.
-	*
-	* @param a percent of alcohol.
-	*/
-	public final void setAlcohol(final double a) {
-		this.alcohol = a;
-	}
+    /**
+    * Method to set the percent of alcohol.
+    *
+    * @param a percent of alcohol.
+    */
+    public final void setAlcohol(final double a) {
+        this.alcohol = a;
+    }
 
-	/**
-	* Method to set the sparkling.
-	*
-	* @param s boolean of sparkling.
-	*/
-	public final void setSparkling(final boolean s) {
-		this.sparkling = s;
-	}
+    /**
+    * Method to set the sparkling.
+    *
+    * @param s boolean of sparkling.
+    */
+    public final void setSparkling(final boolean s) {
+        this.sparkling = s;
+    }
 
-	/**
-	* This method checks if the drink is alcohol-free.
-	*
-	* @return boolean TRUE if there is no alcohol.
-	*/
-	public final boolean getSoft() {
-		// A drink is soft if it contains no alcohol
-		try {
-			double zero = 0.0;
-			int result = Double.compare(this.alcohol, zero);
-			if (result == 0) {
-				return true;
-			}
-			return false;
-		} catch (Exception e) {
-			System.out.println(
-			"Alcohol Comparison Error: Double.compare exception");
-			return false;
-		}
-	}
+    /**
+    * This method checks if the drink is alcohol-free.
+    *
+    * @return boolean TRUE if there is no alcohol.
+    */
+    public final boolean getSoft() {
+        // A drink is soft if it contains no alcohol
+        try {
+            double zero = 0.0;
+            int result = Double.compare(this.alcohol, zero);
+            if (result == 0) {
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            System.out.println(
+            "Alcohol Comparison Error: Double.compare exception");
+            return false;
+        }
+    }
 }
