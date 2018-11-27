62989369e9c8c29121864c8591f2bf1510964e7a
ChristopheSim <c.christophesimon@gmail.com>
2018-11-27 17:41:10 +0100

    Correct classes to validate checkstyle

--- Application/src/main/java/StrategyTrip.java
+++ Application/src/main/java/StrategyTrip.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 
 /**
- * Class to find the trip of bars.
- */
- public class StrategyTrip implements Strategy {
-  /**
-   *  Method solve.
-   *
-   * @param p position of the user.
-   * @param c carcateristics of the user.
-   * @return list of places.
-   */
-   public final ArrayList<Place> solve(final Position p,
-   final Caracteristics c) {
+* Class to find the trip of bars (ordered).
+*/
+public class StrategyTrip implements Strategy {
+    /**
+    *  Method solve.
+    *
+    * @param p position of the user.
+    * @param c carcateristics of the user.
+    * @return ordered list of places.
+    */
+    public final ArrayList<Place> solve(final Position p,
+    final Caracteristics c) {
         ArrayList<Place> lst = new ArrayList<Place>();
         // Ordered list of places that matches with the caracteristics.
         return lst;
